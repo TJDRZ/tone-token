0a5619b9bd0bb1470ac4bcf6a38901013ebd4596
TJ <tjdrzewiecki@gmail.com>
2021-11-16 23:29:14 -0500

    Knob idea fleshed out, using react-dial-knob

--- src/components/Knob.js
+++ src/components/Knob.js
@@ -1,9 +1,27 @@
 import "../styles/Knob.css";
+import { useState } from "react";
+import { Basic } from "react-dial-knob";
 
 function Knob() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="Knob">
-      <div className="notch"></div>
+      <Basic
+        className="BasicKnob"
+        diameter={150}
+        min={0}
+        max={100}
+        step={1}
+        value={value}
+        theme={{
+          defaultColor: "#000",
+          activeColor: "#fada5e",
+          gradientStart: "#bcb499",
+          gradientEnd: "#efd475"
+        }}
+        onValueChange={setValue}
+      ></Basic>
     </div>
   );
 }
